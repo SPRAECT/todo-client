317c94142971fbc5fbcfcd7c8d22a435bcf24e1f
leejin_rho <52371699+leejin-rho@users.noreply.github.com>
2022-08-26 19:28:04 +0900

    📝 기초추가

--- todo-front/src/App.js
+++ todo-front/src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
+import React, {useEffect} from "react";
 import './App.css';
+import axios from "axios";
+
+//useEffect 사용하면 앱이 처음 실행됐을 때 무언가 실행되도록 만들 수 있음
 
 function App() {
+
+  const baseUrl = "http://localhost:8080"
+
+  useEffect(() => {
+    getTodos();
+  }, []); //getTodos가 앱이 실행되자마자 딱 한번만 발생하도록, []를 붙여야 한번만 발생
+  async function getTodos() {
+    /**
+     * 함수이름이랑 url은 임시
+     */
+    await axios
+    .get(baseUrl + "/todo") //데이터 불러오기
+    .then((response) => {  //데이터 불러온 이후에 뭐 할건지
+      console.log(response.data)  // response로 받아온 데이터
+    })
+    .catch((error) => {//데이터 받아오기 실패했을 경우
+      console.error(error)
+    })  
+  }
+
   return (
     <div className="App">
       <header className="App-header">
