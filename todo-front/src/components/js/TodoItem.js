8e7de0ea47f12e961a6054edea00a640cdcc8fe2
leejin_rho <52371699+leejin-rho@users.noreply.github.com>
2022-09-06 03:53:44 +0900

    🔨

--- todo-front/src/components/js/TodoItem.js
+++ todo-front/src/components/js/TodoItem.js
@@ -1,8 +1,12 @@
 import React from "react";
+import {MdCheckCircle, MdCheckBoxOutlineBlank} from "react-icons/md";
 
 const TodoItem = ({todo}) => {
-    const {content} = todo;
-    return <div> {content} </div>;
+    const {id, user_id, content, completed} = todo;
+    return <div className="TodoItem">
+        <div className={`list ${completed ? 'completed' : " "}`}></div>
+        {completed ? <MdCheckCircle/> : <MdCheckBoxOutlineBlank/>}
+    </div>;
 }
 
 export default TodoItem;
